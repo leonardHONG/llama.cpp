02d2934210986f11c7124e44d78d622ca646eaca
梁厚宏 <2695316095@qq.com>
2026-04-29 14:49:43 +0800

    blackwell variant combo: fa128 + threads256 stacked for   DKQ=64,DV=64

--- ggml/src/ggml-cuda/fattn-mma-f16.cuh
+++ ggml/src/ggml-cuda/fattn-mma-f16.cuh
@@ -83,7 +83,11 @@ static constexpr __host__ __device__ fattn_mma_config ggml_cuda_fattn_mma_get_co
 }
 
 static constexpr __host__ __device__ fattn_mma_config ggml_cuda_fattn_mma_get_config_blackwell(const int DKQ, const int DV, const int ncols) {
-    // TODO: tune for sm_120 / sm_121.
+    // variant combo: fa128 + threads256 stacked for DKQ=64 DV=64.
+    GGML_CUDA_FATTN_MMA_CONFIG_CASE( 64,  64,  8, 128, 2, 128,  32,  32,  32, 2, true);
+    GGML_CUDA_FATTN_MMA_CONFIG_CASE( 64,  64, 16, 128, 2, 128,  32,  32,  32, 2, true);
+    GGML_CUDA_FATTN_MMA_CONFIG_CASE( 64,  64, 32, 256, 2, 128,  32,  32,  32, 2, true);
+    GGML_CUDA_FATTN_MMA_CONFIG_CASE( 64,  64, 64, 256, 2, 128,  32,  32,  32, 2, true);
     return ggml_cuda_fattn_mma_get_config_ampere(DKQ, DV, ncols);
 }
 
